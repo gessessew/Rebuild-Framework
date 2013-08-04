3b7cfd0add25ab696140724c5b0b2f86dd3a3294
danylaporte <dany_laporte@hotmail.com>
2013-08-04 17:27:59 -0400

    - Simplify RunnableScheduler Implementation - Add Observable Null Operators extensions

--- Rebuild.NetFx.Rx/Extensions/ObservableExtensions.cs
+++ Rebuild.NetFx.Rx/Extensions/ObservableExtensions.cs
@@ -1,10 +1,27 @@
 ﻿using Rebuild.Rx;
 using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 
 namespace Rebuild.Extensions
 {
     public static class ObservableExtensions
     {
+        public static IObservable<T> EmptyIfNull<T>(this IObservable<T> source, IScheduler scheduler = null)
+        {
+            return source ?? Observable.Empty<T>(scheduler);
+        }
+
+        public static IObservable<T> NeverIfNull<T>(this IObservable<T> source)
+        {
+            return source ?? Observable.Never<T>();
+        }
+
+        public static IObservable<T> ReturnIfNull<T>(this IObservable<T> source, IScheduler scheduler = null, T defaultValue = default(T))
+        {
+            return source ?? Observable.Return<T>(defaultValue, scheduler);
+        }
+
         public static void RunUntilCompleted<T>(this IObservable<T> source, RunnableScheduler scheduler)
         {
             scheduler.RunUntilCompleted(source);
--- Rebuild.NetFx.Rx/RunnableScheduler.cs
+++ Rebuild.NetFx.Rx/RunnableScheduler.cs
@@ -1,214 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Reactive.Concurrency;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Reactive.PlatformServices;
 using System.Threading;
 
 namespace Rebuild.Rx
 {
-    public sealed class RunnableScheduler : LocalScheduler, ISchedulerPeriodic, IDisposable
+    public sealed class RunnableScheduler : IScheduler, ISchedulerPeriodic, IDisposable
     {
-        private IConcurrencyAbstractionLayer _concurrencyAbstractionLayer;
-        private bool _disposed;
-        private readonly SemaphoreSlim _evt;
-        private readonly object _gate;
-        private ScheduledItem<TimeSpan> _nextItem;
-        private readonly SerialDisposable _nextTimer;
-        private readonly SchedulerQueue<TimeSpan> _queue;
-        private readonly Queue<ScheduledItem<TimeSpan>> _readyList;
-        private IStopwatch _stopwatch;
+        private readonly ManualResetEventSlim _resetEvent;
+        private readonly EventLoopScheduler _scheduler;
+        private ThreadStart _threadStart;
 
         public RunnableScheduler()
         {
-            _concurrencyAbstractionLayer = PlatformEnlightenmentProvider.Current.GetService<IConcurrencyAbstractionLayer>();
-            _stopwatch = _concurrencyAbstractionLayer.StartStopwatch();
-            _gate = new object();
-            _evt = new SemaphoreSlim(0);
-            _queue = new SchedulerQueue<TimeSpan>();
-            _readyList = new Queue<ScheduledItem<TimeSpan>>();
-            _nextTimer = new SerialDisposable();
+            _resetEvent = new ManualResetEventSlim();
+            _scheduler = new EventLoopScheduler(ts =>
+            {
+                _threadStart = ts;
+                _resetEvent.Set();
+                return new Thread(() => { });
+            });
         }
 
         public void Dispose()
         {
-            lock (_gate)
-            {
-                if (!_disposed)
-                {
-                    _disposed = true;
-                    _nextTimer.Dispose();
-                    _evt.Release();
-                }
-            }
+            _scheduler.Schedule(_scheduler.Dispose);
+            _resetEvent.Dispose();
         }
 
-        public override IDisposable Schedule<TState>(TState state, TimeSpan dueTime, Func<IScheduler, TState, IDisposable> action)
+        public IDisposable Schedule<TState>(TState state, DateTimeOffset dueTime, Func<IScheduler, TState, IDisposable> action)
         {
-            if (action == null)
-            {
-                throw new ArgumentNullException("action");
-            }
+            return _scheduler.Schedule(state, dueTime, action);
+        }
 
-            var due = _stopwatch.Elapsed + dueTime;
-            var si = new ScheduledItem<TimeSpan, TState>(this, state, action, due);
+        public IDisposable Schedule<TState>(TState state, Func<IScheduler, TState, IDisposable> action)
+        {
+            return _scheduler.Schedule(state, action);
+        }
 
-            lock (_gate)
-            {
-                if (_disposed)
-                {
-                    throw new ObjectDisposedException(GetType().Name);
-                }
-
-                if (dueTime <= TimeSpan.Zero)
-                {
-                    _readyList.Enqueue(si);
-                }
-                else
-                {
-                    _queue.Enqueue(si);
-                }
-                _evt.Release();
-            }
-
-
-            return Disposable.Create(si.Cancel);
+        public IDisposable Schedule<TState>(TState state, TimeSpan dueTime, Func<IScheduler, TState, IDisposable> action)
+        {
+            return _scheduler.Schedule(state, dueTime, action);
         }
 
         public IDisposable SchedulePeriodic<TState>(TState state, TimeSpan period, Func<TState, TState> action)
         {
-            if (period < TimeSpan.Zero)
-            {
-                throw new ArgumentOutOfRangeException("period");
-            }
-
-            if (action == null)
-            {
-                throw new ArgumentNullException("action");
-            }
-
-            var start = _stopwatch.Elapsed;
-            var next = start + period;
-            var state1 = state;
-            var d = new MultipleAssignmentDisposable();
-            var gate = new AsyncLock();
-            var tick = default(Func<IScheduler, object, IDisposable>);
-
-            tick = (self_, _) =>
-            {
-                next += period;
-
-                d.Disposable = self_.Schedule(null, next - _stopwatch.Elapsed, tick);
-
-                gate.Wait(() => state1 = action(state1));
-
-                return Disposable.Empty;
-            };
-
-            d.Disposable = Schedule(null, next - _stopwatch.Elapsed, tick);
-
-            return new CompositeDisposable(d, gate);
+            return _scheduler.SchedulePeriodic(state, period, action);
         }
 
-        public void Run(bool exitIfEmpty = true, CancellationToken token = default(CancellationToken))
+        public void Run()
         {
-            var waitHandles = new WaitHandle[] { _evt.AvailableWaitHandle, token.WaitHandle };
-
-            while (true)
-            {
-                WaitHandle.WaitAny(waitHandles);
-
-                var ready = default(ScheduledItem<TimeSpan>[]);
-
-                lock (_gate)
-                {
-                    if (_disposed)
-                    {
-                        ((IDisposable)_evt).Dispose();
-                        return;
-                    }
-
-                    if (token.IsCancellationRequested)
-                    {
-                        return;
-                    }
-
-                    while (_queue.Count > 0 && _queue.Peek().DueTime <= _stopwatch.Elapsed)
-                    {
-                        _readyList.Enqueue(_queue.Dequeue());
-                    }
-
-                    if (_queue.Count > 0)
-                    {
-                        var next = _queue.Peek();
-
-                        if (next != _nextItem)
-                        {
-                            _nextItem = next;
-                            _nextTimer.Disposable = _concurrencyAbstractionLayer.StartTimer(Tick, next, next.DueTime - _stopwatch.Elapsed);
-                        }
-                    }
-
-                    if (_readyList.Count > 0)
-                    {
-                        ready = _readyList.ToArray();
-                        _readyList.Clear();
-                    }
-                }
-
-                if (ready != null)
-                {
-                    foreach (var item in ready)
-                    {
-                        if (!item.IsCanceled)
-                        {
-                            item.Invoke();
-                        }
-                    }
-                }
-
-                if (exitIfEmpty)
-                {
-                    lock (_gate)
-                    {
-                        if (_readyList.Count == 0 && _queue.Count == 0)
-                        {
-                            return;
-                        }
-                    }
-                }
-            }
+            _resetEvent.Wait();
+            _threadStart();
         }
 
         public void RunUntilCompleted<T>(IObservable<T> observable)
         {
-            var cancellationTokenSource = new CancellationTokenSource();
+            Exception exception = null;
 
             observable
-                .SubscribeOn(TaskPoolScheduler.Default)
-                .Finally(cancellationTokenSource.Cancel)
-                .Subscribe();
+                .Finally(Dispose)
+                .Subscribe(_ => { }, ex => exception = ex);
 
-            Run(false, cancellationTokenSource.Token);
+            Run();
+
+            if (exception != null)
+            {
+                throw exception;
+            }
         }
 
-        private void Tick(object state)
+        public DateTimeOffset Now
         {
-            lock (_gate)
-            {
-                if (!_disposed)
-                {
-                    var item = (ScheduledItem<TimeSpan>)state;
-
-                    if (_queue.Remove(item))
-                    {
-                        _readyList.Enqueue(item);
-                    }
-
-                    _evt.Release();
-                }
-            }
+            get { return _scheduler.Now; }
         }
     }
 }
